ad9c3f40fc605e69cf408ddd082dce691e160892
CJMoye <Cjmoye30@gmail.com>
2023-04-11 13:59:21 -0400

    Note added correctly and shows on the sidebar - MVP met

---
+++ public/routes/notes.js
@@ -0,0 +1,49 @@
+const notesRouter = require('express').Router();
+const { readFromFile, readAndAppend } = require('../../fsUtils');
+
+// import and testing the uuid function/method
+// const { v4: uuidv4 } = require('uuid');
+// console.log(`Here is a test v4 uuid: ${uuid.v4()}`);
+
+notesRouter.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+
+    // res.sendFile(path.join(__dirname, 'public/notes.html'))
+  })
+  
+  // get all notes from the database
+//   notes.get('/api/notes', (req, res) => {
+//     // readFromFile('./db/db.json').then((data) => res.json(data));
+//     res.json(notesData);
+//   })
+  
+  // create const variable to store the body elements (note-title and note-textarea)
+  // I need to actually write this info into the database file - the frontend should already be solved where the save button then regiesters this information
+  
+  // Give each note a unique ID
+  
+  // need to get all the previous tips pulled after the resquest is hit so that they can display on the side of the page
+  notesRouter.post('/', (req, res) => {
+  
+    console.log(`A ${req.method} method was hit!`)
+  
+    let newNote;
+  
+    if(req.body && req.body.title && req.body.text) {
+      newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        // note_id: uuid()
+      };
+  
+      readAndAppend(newNote, `./db/db.json`)
+      res.send();
+    } else {
+      res.json(`error`)
+    }
+  
+    console.log(req.body)
+  
+  })
+
+  module.exports = notesRouter;
--- server.js
+++ server.js
@@ -1,73 +1,27 @@
 // boilerplate requirements
 const express = require('express');
 const path = require('path');
-const { readAndAppend, readFromFile } = require('./fsUtils');
-
-
-// requuired for unique IDs
-const uuid = require('uuid');
-console.log(`Here is a test v1 uuid: ${uuid.v1()}`);
-console.log(`Here is a test v4 uuid: ${uuid.v4()}`);
+// const { readAndAppend, readFromFile } = require('./fsUtils');
+const api = require('./public/routes/index.js')
 
 const PORT = 3001;
-const notesData = require('./db/db.json')
+// const notesData = require('./db/db.json')
 const app = express();
-
-
-// query selectors
-// const saveButton = document.querySelector('.save-note');
-
-// creating variable to use data fron notes db
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
-// Get Routes
+// Get route for the homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Get route for the feedback page
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
-
-// API Routes
-// get all notes from the database
-app.get('/api/notes', (req, res) => {
-  // readFromFile('./db/db.json').then((data) => res.json(data));
-  res.json(notesData);
-})
-
-// create const variable to store the body elements (note-title and note-textarea)
-// I need to actually write this info into the database file - the frontend should already be solved where the save button then regiesters this information
-
-// Give each note a unique ID
-
-// need to get all the previous tips pulled after the resquest is hit so that they can display on the side of the page
-app.post('/api/notes', (req, res) => {
-
-  console.log(`A ${req.method} method was hit!`)
-
-  let newNote;
-
-  if(req.body && req.body.title && req.body.text) {
-    newNote = {
-      title: req.body.title,
-      text: req.body.text,
-      note_id: uuid()
-    };
-
-    readAndAppend(newNote, `./db/db.json`)
-    res.send();
-  } else {
-    res.json(`error`)
-  }
-
-  console.log(req.body)
-
-})
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
